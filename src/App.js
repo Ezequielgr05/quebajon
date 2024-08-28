a360db7ed488af7aeb6a48879e466bb2164b9552
Ezequielgr05 <gomezrossiezequiel@gmail.com>
2024-08-28 20:59:26 -0300

    testing ant design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Alert, Card } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">hola</Button>
+      <Alert showIcon message="Success Text" type="info" closable description="" />
+      <Card title="Something" extra={<a href="#">More</a>}>
+          <p>hola</p>
+      </Card>
+      
     </div>
   );
 }
