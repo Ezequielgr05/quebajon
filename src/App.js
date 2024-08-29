f9c68a16424d1eb5d2d8d7af263698163545a38d
Ezequielgr05 <gomezrossiezequiel@gmail.com>
2024-08-28 21:46:19 -0300

    changes somethings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import { Button, Alert, Card } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary">hola</Button>
-      <Alert showIcon message="Success Text" type="info" closable description="" />
-      <Card title="Something" extra={<a href="#">More</a>}>
-          <p>hola</p>
-      </Card>
+    <div>
       
     </div>
   );
