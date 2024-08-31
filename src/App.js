d8229cefcca91135954de53ee763ae674d98d153
Ezequielgr05 <gomezrossiezequiel@gmail.com>
2024-08-31 18:02:36 -0300

    i forgot something

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import './assets/css/style.css'
 import './assets/fonts/stylesheet.css'
-import Saludo from './components/Saludo'
 
 function App() {
   return (
     <div>
-        <Saludo darkmode={true}/>
     </div>
   );
 }
