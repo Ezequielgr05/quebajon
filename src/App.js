5f355505979cd7c01a921aa72be0b3dadd94445c
Ezequielgr05 <gomezrossiezequiel@gmail.com>
2024-08-31 18:01:42 -0300

    fix some errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import './assets/css/style.css'
 import './assets/fonts/stylesheet.css'
+import Saludo from './components/Saludo'
 
 function App() {
   return (
     <div>
+        <Saludo darkmode={true}/>
     </div>
   );
 }
