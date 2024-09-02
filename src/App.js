1a16e51771880935995020f670b9fc57de52cb6b
Ezequielgr05 <gomezrossiezequiel@gmail.com>
2024-09-02 16:23:27 -0300

    create component "Ingrediente" 1/2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import './assets/css/style.css'
 import './assets/fonts/stylesheet.css'
-import Logo from './components/Logo';
+import Ingredientes from './components/Ingredientes';
+import Saludo from './components/Saludo'
 
 function App() {
   return (
     <div>
-      <Logo/>
+      <Ingredientes title={"huevo"} type={"light"} size={"lg"} darkmode={false}/>
+      <Ingredientes title={"huevo"} type={"dark"} size={"sm"} darkmode={true}/>
+      <Saludo/>
     </div>
   );
 }
-
 
 export default App;
--- src/components/Ingredientes.js
+++ src/components/Ingredientes.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../components-css/Ingredientes.css'
+import Logo from '../assets/imagenes/logo1.png'
 
-function Ingredientes({title, src, alt}) {
+function Ingredientes({title, type, size, darkmode=false}) {
+  let classIcon = `icon-${size}-${type}`;
+
+  if (darkmode) {
+    classIcon = `icon-${size}-${type} darkmode`;
+  }
+  
+  
   return (
-    <div>
-        <div><img src={src} alt={alt} /></div>
+    <div className={classIcon}>
+        <div><img src={Logo} alt={'hola'} /></div>
         <p>{title}</p>
     </div>
   )
